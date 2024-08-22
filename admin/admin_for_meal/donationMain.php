1bbf5c6d7a0fa0c4058a7eb126d3d41a7ad0e946
ZaiYong36 <139421497+ZaiYong36@users.noreply.github.com>
2024-08-22 15:01:10 +0800

    meal

--- admin/admin_for_meal/donationMain.php
+++ admin/admin_for_meal/donationMain.php
@@ -1,6 +1,7 @@
 <?php
-    session_start();
+    include "../../components/db_connect.php";
     include "../admin_for_meal/header.php"; 
+    session_start();
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +9,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $pageTitle ?? 'Default Title'; ?></title>
-    <link rel="icon" type="image/x-icon" href="/mips/images/MIPS_icon.png">
+    <title>Donation Page</title>
+    <link rel="icon" type="image/x-icon" href="/mips/admin/images/MIPS_icon.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -17,8 +18,11 @@
     <link rel="stylesheet" href="../admin_for_meal/base.css">
     <link rel="stylesheet" href="../admin_for_meal/common.css">
     <link rel="stylesheet" href="../admin_for_meal/admin.css">
+    <link rel="stylesheet" href="../admin_for_meal/donationMain.css">
 </head>
 <body>
     <script src="../admin_for_meal/admin.js"></script>
+    <button id="btn">Donation Info</button>
+    <button id="btn1">Add Event</button>
 </body>
 </html>
